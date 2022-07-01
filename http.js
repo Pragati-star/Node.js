e2a878102467cbafb2e9b086ce95e8b5707aff48
PRAGATI <pragati2004muz@gmail.com>
2022-07-01 12:15:05 +0530

    MAIN

--- http.js
+++ http.js
@@ -1,9 +1,9 @@
 var http = require('http');
-// var port=process.env.PORT || 8657
+var port=process.env.PORT || 8657
 
 var server=http.createServer((req,res)=> {
     res.write("<h1>Hii from NodeJs Server</h1>");
     res.end();
 })
 
-server.listen(8657)
+server.listen(port)
